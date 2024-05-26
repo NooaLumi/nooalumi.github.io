20334f12c4a7bd6aa6a1050e25c69d12e0a8737c
NooaLumi <nooa.lumilaakso@gmail.com>
2024-05-26 21:12:10 +0300

    fix: reduce code snippet font size

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,9 +1,9 @@
 // custom typefaces
 import '@fontsource/poppins';
 import '@fontsource/source-serif-pro';
+// Highlighting for code blocks
+import "prismjs/themes/prism-tomorrow.css"
 // milligram css framework
 import "./src/milligram.css"
 // custom global styles
 import "./src/main.css"
-// Highlighting for code blocks
-import "prismjs/themes/prism-tomorrow.css"
