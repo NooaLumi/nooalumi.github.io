e4403925e1b680a43d15edb5d1e611fc7c558c4a
NooaLumi <nooa.lumilaakso@gmail.com>
2024-05-26 16:15:03 +0300

    Updates

--- gatsby-browser.tsx
+++
@@ -1,9 +0,0 @@
-// custom typefaces
-import '@fontsource/poppins';
-import '@fontsource/source-serif-pro';
-// milligram css framework
-import "./src/milligram.css"
-// custom global styles
-import "./src/main.css"
-// Highlighting for code blocks
-import "prismjs/themes/prism-tomorrow.css"
